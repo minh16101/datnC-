347ceb2cbb5ba53a20df7cdf83b6c5607035d019
Minh Hoang Nhat <Admin@LAPTOP-F6J06I0S.(none)>
2023-02-27 00:41:49 +0700

    first

---
+++ DATN/DATN/Controllers/BookingHotelController.cs
@@ -0,0 +1,30 @@
+﻿using DATN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DATN.Controllers
+{
+    public class BookingHotelController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: BookingHotel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ItemPartialView()
+        {
+            var items = db.BookingRoom.Where(x => x.BookingHotelId == 2).Take(5).ToList();
+            foreach(var i in items)
+            {
+                var itemImages = db.ImageRoom.Where(x => x.BookingRoomId == i.Id).FirstOrDefault();
+                i.ImageRoom.Add(itemImages);
+            }
+            return PartialView(items);
+        }
+    }
+}
